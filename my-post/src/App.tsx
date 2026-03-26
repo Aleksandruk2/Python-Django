a5ccf1b9340ce2c4a1b0acbe93d886c50db0a81c
Aleksandruk2 <aaleksandruk2018@gmail.com>
2026-03-26 02:09:00 +0200

    add list of city

--- my-post/src/App.tsx
+++ my-post/src/App.tsx
@@ -1,121 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import {useEffect, useState} from "react";
+import type {ICity} from "./interfaces/city/ICity.ts";
+import APP_ENV from "./env";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [cities, setCities] = useState<ICity[]>([])
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    useEffect(() => {
+        const url = `${APP_ENV.API_BASE_URL}/api/cities`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                setCities(data);
+            });
+    },[]);
 
-      <div className="ticks"></div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+    return (
+        <>
+            <div className="mt-2 w-full flex justify-around flex-wrap">
+                {cities.map(city => (
+                    <div key={city.id} className="transition-transform duration-300 ease-out hover:scale-102 p-2 mt-5 border-gray-200 dark:border-gray-700 ">
+                        <div className="relative flex w-80 flex-col rounded-xl animate-gradient bg-gradient-to-tr from-gray-900  to-gray-700 dark:bg-gray-800 bg-clip-border text-gray-200" style={{boxShadow: "0px 0px 20px -3px rgba(0, 0, 0, 0.3)"}} >
+                            <div className="relative">
+                                <div className=" border-b border-gray-400 flex justify-center items-center relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl text-white shadow-lg myBGImage">
+                                    <img draggable={false}
+                                         className="w-full"
+                                         style={{boxShadow: "0px 0px 30px 5px rgba(0, 0, 0, 0.3)"}}
+                                         src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW-6S_OE_jaSX52m553IvRJpZ6EYA5-3yWkg&s`}
+                                         alt={city.name}/>
+                                </div>
+                                <div className="absolute px-4 mb-2.5 bottom-0">
+                                    <div className="px-2 pb-2 p-1 bg-black/50 rounded-r-xl">
+                                        <h5 className="block font-sans text-xl font-semibold leading-snug tracking-normal antialiased">
+                                            {city.name}
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="p-5 pb-0">
+                                <div className="flex justify-between">
+                                    <p className="line-clamp-1 bg-blue-500 border-2 border-blue-700 rounded-lg px-1 mb-2 font-sans font-semibold text-base leading-snug tracking-normal text-gray-100">
+                                        {city.description}
+                                    </p>
+                                </div>
+                            </div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default App
+
+
