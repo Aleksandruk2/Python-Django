61f3373ad09479507bbb442437edb9892cca6405
Aleksandruk2 <aaleksandruk2018@gmail.com>
2026-04-02 17:21:05 +0300

    added file.sh *

--- django-api/post-api/myapi/settings.py
+++ django-api/post-api/myapi/settings.py
@@ -64,14 +64,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",
-#     "http://127.0.0.1:5173",
-#     "http://localhost:4513",
-#     "http://127.0.0.1:4513",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:4513",
+    "http://127.0.0.1:4513",
+]
 
 ROOT_URLCONF = 'myapi.urls'
 
