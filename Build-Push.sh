0e969859023be0715e57135455e6613c38c53eaa
Aleksandruk2 <aaleksandruk2018@gmail.com>
2026-04-02 16:52:11 +0300

    mod file.sh

--- Build-Push.sh
+++ Build-Push.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -ex  # -e зупиняє скрипт при помилці, -x покаже хід виконання
 
-# ==== WEB ====
-# cd my-transfer-ts/transfer-front
-# docker build -t transfer-react --build-arg VITE_API_BASE_URL=https://av-transfer.itstep.click .
-# docker tag transfer-react:latest avalentyn/transfer-react:latest
-# docker push avalentyn/transfer-react:latest
-# echo "Done ---client---!"
 
 # ==== API ====
 cd django-api/post-api
@@ -15,5 +9,13 @@ docker tag django-api:latest avalentyn/django-api:latest
 docker push avalentyn/django-api:latest
 
 echo "Done ---api---!"
+
+# ==== WEB ====
+cd ../../my-post
+docker build -t my-post --build-arg VITE_API_BASE_URL=http://localhost:4512 .
+docker tag my-post:latest avalentyn/my-post:latest
+docker push avalentyn/my-post:latest
+
+echo "Done ---client---!"
 
 #read -p "Press any key to exit..."
